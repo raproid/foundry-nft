5ff7556d6f3a83daa03443ccb2b9fb8160485de3
Ole Sorensen <raproid@gmail.com>
2025-07-19 03:28:34 +0300

    update MoodNFT.sol, start writing unit test for MoodNFT.sol

---
+++ script/DeployMoodNft.s.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+import {Script, console} from "forge-std/Script.sol";
+import {MoodNft} from "src/MoodNft.sol";
+import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
+
+contract DeployMoodNft is Script {
+    function run() external returns (MoodNft) {
+        string memory seductiveSvg = vm.readFile("img/shoosh/shoosh_cl_27kb.svg");
+        string memory playfulSvg = vm.readFile("img/shoosh/shoosh_bl_24kb.svg");
+
+        vm.startBroadcast();
+        MoodNft moodNft = new MoodNft(
+            svgToImageURI(seductiveSvg),
+            svgToImageURI(playfulSvg)
+        );
+        vm.stopBroadcast();
+
+        return moodNft;
+    }
+
+    function svgToImageURI(string memory svg) public pure returns (string memory) {
+        string memory baseURL = "data:image/svg+xml;base64,";
+        string memory svgBase64Encoded = Base64.encode(bytes(string(abi.encodePacked(svg))));
+        return string(abi.encodePacked(baseURL, svgBase64Encoded));
+    }
+}
--- src/MoodNft.sol
+++ src/MoodNft.sol
@@ -5,7 +5,11 @@ pragma solidity ^0.8.19;
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
 
-contract MoodNFT is ERC721 {
+contract MoodNft is ERC721 {
+    /* ERRORS */
+    error MoodNft__CantFlipMoodIfNotOwner();
+    error MoodNft__InvalidTokenId(uint256);
+
     uint256 private s_tokenCounter;
     string private s_seductiveSvImageUri;
     string private s_playfulSvgImageUri;
@@ -16,7 +20,7 @@ contract MoodNFT is ERC721 {
 
     mapping(uint256 => Mood) private s_tokenIdToMood;
 
-    constructor(string memory seductiveSvImageUri, string memory playfulSvgImageUri) ERC721("MoodNFT", "MNFT") {
+    constructor(string memory seductiveSvImageUri, string memory playfulSvgImageUri) ERC721("MoodNft", "MNFT") {
         s_tokenCounter = 0;
         s_seductiveSvImageUri = seductiveSvImageUri;
         s_playfulSvgImageUri = playfulSvgImageUri;
@@ -29,7 +33,7 @@ contract MoodNFT is ERC721 {
     }
 
     function _baseURI() internal pure override returns (string memory) {
-        return "data:application/json;base64";
+        return "data:application/json;base64,";
     }
 
     function tokenURI(uint256 tokenId) public view override returns (string memory) {
@@ -40,18 +44,34 @@ contract MoodNFT is ERC721 {
             imageURI = s_playfulSvgImageUri;
         }
 
-        return
-        string(
+        return string(
             abi.encodePacked(
                 _baseURI(),
-                    Base64.encode(
-                        bytes(
-                            abi.encodePacked(
-                                '{"name": "', name(), '", "description": "An  NFT that changes the mood", "attributes": [{"trait_type": "charm", "value": 101}], "image": "', imageURI, '"}'
-                            )
+                Base64.encode(
+                    bytes(
+                        abi.encodePacked(
+                            '{"name":"',
+                            name(),
+                            '", "description":"An  NFT that changes the mood", ',
+                            '"attributes": [{"trait_type": "charm", "value": 100}], "image":"',
+                            imageURI,
+                            '"}'
                         )
                     )
                 )
-            );
+            )
+        );
+    }
+
+    function flipMood(uint256 tokenId) public {
+        // Only the owner can change the mood
+        if(!_isAuthorized(ownerOf(tokenId), msg.sender, tokenId)) {
+            revert MoodNft__CantFlipMoodIfNotOwner();
+        }
+        if (s_tokenIdToMood[tokenId] == Mood.SEDUCTIVE) {
+            s_tokenIdToMood[tokenId] = Mood.PLAYFUL;
+        } else {
+            s_tokenIdToMood[tokenId] = Mood.SEDUCTIVE;
+        }
     }
 }
